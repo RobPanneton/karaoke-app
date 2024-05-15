d257b256e4a8431f024eaf2e52f3d64982bfd308
RobPanneton <robert.panneton@gmail.com>
2024-05-14 20:43:33 -0400

    connect context to app and api, add types

--- app/src/context/TranscriptContext.tsx
+++ app/src/context/TranscriptContext.tsx
@@ -1,10 +1,65 @@
-import React, { createContext, useContext, useState } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import {
+  CurrentTranscript,
+  TranscriptContext,
+  TranscriptItemList,
+} from "../types/transcriptTypes";
 
-export const TransacriptContext = createContext(null);
+export const TransacriptContext = createContext<TranscriptContext | null>(null);
 
-export const SearchProvider: React.FC = ({}) => {
+export const TranscriptProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [transcriptList, setTranscriptList] =
+    useState<TranscriptItemList | null>(null);
+  const [currentTranscript, setCurrentTranscript] =
+    useState<CurrentTranscript | null>(null);
+  const [listError, setListError] = useState<null | any>(null);
+  const [transcriptError, setTranscriptError] = useState<null | any>(null);
+
+  const apiURL = process.env.REACT_APP_TRANSCRIPT_API_URL;
+
+  const getTranscriptData = useCallback(
+    async (
+      endpoint: string,
+      setFn:
+        | Dispatch<SetStateAction<TranscriptItemList | null>>
+        | Dispatch<SetStateAction<CurrentTranscript | null>>,
+      setErrorFn: Dispatch<SetStateAction<any>>
+    ) => {
+      console.log("fetching data");
+      try {
+        const res = await fetch(`${apiURL}${endpoint}`);
+        const data = await res.json();
+        console.log({ data });
+        setFn(data);
+      } catch (e) {
+        console.log({ e });
+        setErrorFn(e);
+      }
+    },
+    [apiURL]
+  );
+
+  useEffect(() => {
+    console.log("use effect triggered");
+    getTranscriptData("/transcripts", setTranscriptList, setListError);
+  }, [getTranscriptData]);
+
   return (
-    <TransacriptContext.Provider value={null}>{}</TransacriptContext.Provider>
+    <TransacriptContext.Provider
+      value={{ transcriptList, currentTranscript, listError, transcriptError }}
+    >
+      {children}
+    </TransacriptContext.Provider>
   );
 };
 
---
+++ app/src/types/transcriptTypes.ts
@@ -0,0 +1,36 @@
+export type TranscriptContext = {
+  transcriptList: TranscriptItemList | null;
+  currentTranscript: CurrentTranscript | null;
+  listError: boolean;
+  transcriptError: boolean;
+};
+
+export type TranscriptListItem = {
+  id: Number;
+  name: String;
+};
+
+export type TranscriptItemList = TranscriptListItem[];
+
+export type CurrentTranscript = {
+  id: number;
+  name: string;
+  audio_url: string;
+  comment: string;
+  paragraphs: Paragraph[];
+  words: Word[];
+};
+
+export type Paragraph = {
+  id: string;
+  time: number;
+  duration: number;
+  spearker_id: string;
+};
+
+export type Word = {
+  time: number;
+  duration: number;
+  text: string;
+  paragraph_id: string;
+};
