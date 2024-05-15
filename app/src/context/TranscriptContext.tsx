f6f633dccc4823555fb82d38913e88ea80e01159
RobPanneton <robert.panneton@gmail.com>
2024-05-14 22:07:10 -0400

    cleanup context and add more error handling

--- app/src/context/TranscriptContext.tsx
+++ app/src/context/TranscriptContext.tsx
@@ -10,11 +10,11 @@ import React, {
 import {
   CurrentTranscript,
   SetTranscriptDataFn,
-  TranscriptContext,
+  TranscriptContext as ITranscriptContext,
   TranscriptItemList,
 } from "../types/transcriptTypes";
 
-export const TransacriptContext = createContext<TranscriptContext | null>(null);
+export const TranscriptContext = createContext<ITranscriptContext | null>(null);
 
 export const TranscriptProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -28,6 +28,10 @@ export const TranscriptProvider: React.FC<{ children: React.ReactNode }> = ({
 
   const apiURL = process.env.REACT_APP_TRANSCRIPT_API_URL;
 
+  if (!apiURL) {
+    throw new Error("REACT_APP_TRANSCRIPT_API_URL is not defined");
+  }
+
   const getTranscriptData = useCallback(
     async (
       endpoint: string,
@@ -36,7 +40,13 @@ export const TranscriptProvider: React.FC<{ children: React.ReactNode }> = ({
     ) => {
       console.log("fetching data");
       try {
+        // fetch data from provided endpoint
         const res = await fetch(`${apiURL}${endpoint}`);
+
+        // throw error if request completes but returns non-2xx status code
+        if (!res.ok) throw new Error(`Error fetching data: ${res?.statusText}`);
+
+        // parse data and set to state
         const data = await res.json();
         console.log({ data });
         setDataFn(data);
@@ -51,10 +61,14 @@ export const TranscriptProvider: React.FC<{ children: React.ReactNode }> = ({
 
   const handleUserSelect = (e: React.MouseEvent<HTMLButtonElement>) => {
     const id = e?.currentTarget.value;
+
+    // handle no id being sent to function
     if (!id) {
       setTranscriptError(true);
       return;
     }
+
+    // get selected transcript data: call method to fetch it and set to state
     getTranscriptData(
       `/transcripts/${id}`,
       setCurrentTranscript,
@@ -68,7 +82,7 @@ export const TranscriptProvider: React.FC<{ children: React.ReactNode }> = ({
   }, [getTranscriptData]);
 
   return (
-    <TransacriptContext.Provider
+    <TranscriptContext.Provider
       value={{
         transcriptList,
         currentTranscript,
@@ -78,14 +92,16 @@ export const TranscriptProvider: React.FC<{ children: React.ReactNode }> = ({
       }}
     >
       {children}
-    </TransacriptContext.Provider>
+    </TranscriptContext.Provider>
   );
 };
 
 export const useTranscriptContext = () => {
-  const context = useContext(TransacriptContext);
+  const context = useContext(TranscriptContext);
   if (context === null)
-    throw new Error("useSearchContext must be used within an AppProvider");
+    throw new Error(
+      "useTranscriptContext must be used within a TranscriptProvider"
+    );
 
   return context;
 };
